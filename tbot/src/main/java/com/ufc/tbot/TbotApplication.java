d84f1a24ce368cb6ea61147fb61966062008077c
Anton <antjony98@gmail.com>
2020-02-10 19:33:39 +0000

    Добавление сервиса для логов и его конфигуратора (AppController)

--- tbot/src/main/java/com/ufc/tbot/TbotApplication.java
+++ tbot/src/main/java/com/ufc/tbot/TbotApplication.java
@@ -1,5 +1,6 @@
 package com.ufc.tbot;
 
+import com.ufc.tbot.controller.AppController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TbotApplication {
 
 	public static void main(String[] args) {
+		AppController appController = new AppController();
 		SpringApplication.run(TbotApplication.class, args);
 	}
 
---
+++ tbot/src/main/java/com/ufc/tbot/controller/AppController.java
@@ -0,0 +1,24 @@
+package com.ufc.tbot.controller;
+
+import com.ufc.tbot.TbotApplication;
+import com.ufc.tbot.service.LoggerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private LoggerService loggerService;
+
+    @PostConstruct
+    public void init() {
+        Logger logger = loggerService.getLogger(AppController.class.getName(), true);
+        logger.info("test 1");
+    }
+
+}

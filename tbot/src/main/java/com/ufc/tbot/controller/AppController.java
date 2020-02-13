ed5d8ea7ba9eabdfcc3af17a12a58ca0e4c0fcfb
Anton <antjony98@gmail.com>
2020-02-13 15:51:26 +0000

    Убрать ненужный Log из AppController

---
+++ tbot/src/main/java/com/ufc/tbot/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.ufc.tbot.controller;
+
+import com.ufc.tbot.service.LoggerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private LoggerService loggerService;
+
+    @PostConstruct
+    public void init() {
+        Logger logger = loggerService.getLogger(AppController.class.getName(), true);
+    }
+
+}

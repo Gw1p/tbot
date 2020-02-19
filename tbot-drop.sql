b6dba0c88d3f920b10c5175c608375f0cee4899d
Anton <antjony98@gmail.com>
2020-02-19 15:58:26 +0000

    Добавление .sql чтобы дропнуть все таблицы

---
+++ tbot-drop.sql
@@ -0,0 +1,20 @@
+DROP TABLE MessagesOut;
+GO
+
+DROP TABLE MessagesIn;
+GO
+
+DROP TABLE UserChats;
+GO
+
+DROP TABLE Chats;
+GO
+
+DROP TABLE UserPermissions;
+GO
+
+DROP TABLE Permissions;
+GO
+
+DROP TABLE Users;
+GO

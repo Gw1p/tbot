81d31c28fb09e3e16ada477a6a6d82cafcbee042
Anton <antjony98@gmail.com>
2020-02-19 13:24:10 +0000

    Обновление таблиц для восприятия иностранных символов

--- tbot.sql
+++ tbot.sql
@@ -1,40 +1,57 @@
 CREATE TABLE Users(
 	id bigint NOT NULL PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    username VARCHAR(255),
-	firstMessage DATETIME NOT NULL,
-	approved BIT NOT NULL DEFAULT(0)
+    firstName NVARCHAR(255),
+    lastName NVARCHAR(255),
+    username NVARCHAR(255),
+	firstMessage DATETIME NOT NULL
 );
 GO
 
-CREATE UNIQUE INDEX appr_usr_indx ON Users (approved);
+CREATE TABLE Permissions(
+	id int NOT NULL PRIMARY KEY,
+	permission NVARCHAR(255) NOT NULL
+);
+GO
+
+INSERT INTO Permissions (id, permission) VALUES (1, 'ADMIN');
+GO
+
+INSERT INTO Permissions (id, permission) VALUES (2, 'USER');
+GO
+
+CREATE TABLE UserPermissions(
+	id int NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	userId bigint NOT NULL FOREIGN KEY REFERENCES Users(id),
+	permissionId int NOT NULL FOREIGN KEY REFERENCES Permissions(id),
+	UNIQUE(userId, 	permissionId)
+);
 GO
 
 CREATE TABLE Chats(
 	id bigint NOT NULL PRIMARY KEY,
-    chatType VARCHAR(10) NOT NULL, -- Private, group, supergroup, channel
-	firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    username VARCHAR(255),
-    title VARCHAR(255),
+    chatType NVARCHAR(10) NOT NULL, -- Private, group, supergroup, channel
+	firstName NVARCHAR(255),
+    lastName NVARCHAR(255),
+    username NVARCHAR(255),
+    title NVARCHAR(255),
 	inviteLink VARCHAR(255)
 );
 GO
 
 -- Какие есть чаты с определенным пользователем
 CREATE TABLE UserChats(
-	id bigint NOT NULL PRIMARY KEY,
+	id int NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	userId bigint NOT NULL FOREIGN KEY REFERENCES Users(id),
 	chatId bigint NOT NULL FOREIGN KEY REFERENCES Chats(id),
-	chatType VARCHAR(255) NOT NULL, -- Private, group, supergroup, channel
-	discoveredDate DATETIME NOT NULL
+	chatType NVARCHAR(255) NOT NULL, -- Private, group, supergroup, channel
+	discoveredDate DATETIME NOT NULL,
+	UNIQUE(userId, chatId)
 );
 GO
 
 CREATE TABLE MessagesIn(
 	id bigint NOT NULL PRIMARY KEY,
-	message VARCHAR(255),
+	message NVARCHAR(max) NOT NULL,
 	messageDate DATETIME NOT NULL,
 	chatId bigint NOT NULL FOREIGN KEY REFERENCES Chats(id),
 	userId bigint NOT NULL FOREIGN KEY REFERENCES Users(id)
@@ -44,7 +61,7 @@ GO
 CREATE TABLE MessagesOut(
 	id bigint NOT NULL PRIMARY KEY,
 	chatId bigint NOT NULL FOREIGN KEY REFERENCES Chats(id),
-	message VARCHAR(255),
+	message NVARCHAR(max),
 	messageDate DATETIME,
 	sent BIT NOT NULL DEFAULT(0)
 );
---
+++ tbot/src/main/java/com/ufc/tbot/model/PermissionType.java
@@ -0,0 +1,4 @@
+package com.ufc.tbot.model;
+
+public enum PermissionType {
+}
